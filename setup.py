a63d1b875f295077b4ef32bd051f4a59feb0d769
Sidcool0311-ml <sidcool0311@gmail.com>
2026-08-28 18:47:28 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+from typing import List
+setup(
+    name="skyguard-ai",
+    version="0.0.1",
+    author="Siddhant",
+    author_email="sidcool0311@gmail.com",
+    packages=find_packages(),
+)
